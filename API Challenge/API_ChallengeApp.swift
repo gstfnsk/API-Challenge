0b4c261861f519742f82cd271442323e188de64e
gstfnsk <giulia.chimini@gmail.com>
2025-08-13 15:51:12 -0300

    create project

---
+++ API Challenge/API_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ChallengeApp.swift
+//  API Challenge
+//
+//  Created by Giulia Stefainski on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct API_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
