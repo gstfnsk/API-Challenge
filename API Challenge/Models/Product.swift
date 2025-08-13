d9dd5110cadc46d1ed962bd7d664403e096e6ba5
gstfnsk <giulia.chimini@gmail.com>
2025-08-13 16:50:12 -0300

    create product struct

---
+++ API Challenge/Models/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  API Challenge
+//
+//  Created by Giulia Stefainski on 13/08/25.
+//
+
+struct Product: Identifiable, Decodable {
+    var id: Int
+    var title: String
+    var description: String
+    var category: String
+    var price: Double
+    var thumbnail: String
+}
+
