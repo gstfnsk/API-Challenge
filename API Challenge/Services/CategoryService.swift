cd02b43b257beb5948c57506a3d3c5bce3de0e0c
enzotonatto <enzotonatto@gmail.com>
2025-08-15 13:44:14 -0300

    fix category service

--- API Challenge/Services/CategoryService.swift
+++ API Challenge/Services/CategoryService.swift
@@ -12,10 +12,13 @@ final class CategoryService: CategoryServiceProtocol {
     
     func fetchCategories() async throws -> [ProductCategory] {
         let urlString = "\(baseURL)/products/category-list"
+        
         guard let url = URL(string: urlString) else { throw URLError(.badURL) }
         let (data, response) = try await URLSession.shared.data(from: url)
+        
         guard let http = response as? HTTPURLResponse, http.statusCode == 200 else { throw URLError(.badServerResponse) }
         let slugs = try JSONDecoder().decode([String].self, from: data)
+        
         return slugs.compactMap(ProductCategory.init(rawValue:))
     }
 }
