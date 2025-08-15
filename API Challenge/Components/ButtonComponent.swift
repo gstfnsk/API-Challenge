786476345fd6ce8abe194dca3553710b9dff3ae1
gstfnsk <giulia.chimini@gmail.com>
2025-08-15 14:58:42 -0300

    button component

---
+++ API Challenge/Components/ButtonComponent.swift
@@ -0,0 +1,35 @@
+//
+//  ButtonComponent.swift
+//  API Challenge
+//
+//  Created by Giulia Stefainski on 15/08/25.
+//
+//
+import SwiftUI
+
+struct ButtonComponent: View {
+    
+    var title: String
+    var action: ()
+
+    var body: some View {
+        
+        Button {
+            action
+        } label: {
+            Text(title)
+                .font(.system(.body, weight: .semibold))
+                .foregroundColor(Color(.labelsPrimary))
+                .frame(maxWidth: .infinity)
+                .frame(height: 54)
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundStyle(.fillsTertiary))
+
+        }.padding(.horizontal, 16)
+    }
+}
+
+        #Preview {
+            ButtonComponent(title:"Add to cart", action:())
+        }
