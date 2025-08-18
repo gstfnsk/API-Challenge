ddf54f9256929b7ab183e002149867360a9b45bf
gstfnsk <giulia.chimini@gmail.com>
2025-08-18 16:43:22 -0300

    create models

---
+++ API Challenge/Models/Cart.swift
@@ -0,0 +1,17 @@
+//
+//  Cart.swift
+//  API Challenge
+//
+//  Created by Giulia Stefainski on 18/08/25.
+//
+
+import SwiftData
+
+@Model
+class Cart {
+    var product: [Int : Int] // id do produto : quantidade
+    
+    init(product: [Int : Int]) {
+        self.product = product
+    }
+}
---
+++ API Challenge/Models/Favorites.swift
@@ -0,0 +1,17 @@
+//
+//  Favorites.swift
+//  API Challenge
+//
+//  Created by Giulia Stefainski on 18/08/25.
+//
+
+import SwiftData
+
+@Model
+class Favorites {
+    var id: Int
+    
+    init(id: Int) {
+        self.id = id
+    }
+}
