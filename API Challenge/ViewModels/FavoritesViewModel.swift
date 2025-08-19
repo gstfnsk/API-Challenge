eab074afb72e2bf076b3eeaea4bdc21946a0ea67
gstfnsk <giulia.chimini@gmail.com>
2025-08-19 16:20:32 -0300

    add productId to favorites

--- API Challenge/ViewModels/FavoritesViewModel.swift
+++ API Challenge/ViewModels/FavoritesViewModel.swift
@@ -15,31 +15,19 @@ class FavoritesViewModel: Observable {
     init(dataSource: SwiftDataService) {
         self.dataSource = dataSource
         
-        // Add dummy expenses to the SwiftData to see if fetching data is works
-        let dummyFavorites = Favorites.dummyFavorites
-        for favorite in dummyFavorites {
-            dataSource.addFavorites(favorite)
-        }
-        
         favorites = dataSource.fetchFavorites()
     }
     
-    func addSampleFavorite() {
-        let favorite = Favorites(id: 100)
+    func addFavoriteById(id: Int) {
+        let favorite = Favorites(id: id)
         dataSource.addFavorites(favorite)
-        
-        // Manually fetch the latest expenses after add new expense
         favorites = dataSource.fetchFavorites()
+        
+        favorites.forEach { favorite in
+            print(favorite.id)
+        }
+        
+        
     }
-}
-
-
-extension Favorites {
-    static let dummyFavorites: [Favorites] = [
-        Favorites(id: 1),
-        
-        Favorites(id: 2),
-        
-        Favorites(id: 3),
-    ]
+    
 }
